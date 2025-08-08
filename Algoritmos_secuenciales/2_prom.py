a29cf12f7eb2ed251a8bb62087c65128ae78c60d
Braian <braianoquen@gmail.com>
2025-08-08 18:28:58 -0500

    2_prom.py

---
+++ Algoritmos_secuenciales/2_prom.py
@@ -0,0 +1,12 @@
+#Sistema para calcular el promedio según los número que ingrese el usuario, usare el while para la cantidad de números
+
+
+print("Bienvenido al sistema de cálculo para promedios")
+
+cantidad = int(input("Cuántos números vas a ingresar: "))
+contador = 1
+while contador <= cantidad :
+    
+    nums= float(input("Ingresa un número: "))
+    
+    contador += 1
