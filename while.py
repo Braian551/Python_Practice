77155645fb4cf6debbf90fb73a1daa8d1c6ea6c0
Braian <braianoquen@gmail.com>
2025-08-08 13:19:23 -0500

    practica 1

--- while.py
+++ while.py
@@ -11,4 +11,17 @@ while contador < 10:
 
 
 fin = time.time()
-print("Tiempo transcurrido")
+
+
+print("Tiempo transcurrido:", fin-inicio  , "segundos")
+
+
+
+
+inicio= time.time()
+
+
+for contador in range(10):
+    print(contador)
+fin = time.time()
+print("Tiempo transcurrido:", fin-inicio, "segundos")
