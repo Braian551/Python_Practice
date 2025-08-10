99faf74259c4bb82f9024c7522041185f04a4a8f
Braian <braianoquen@gmail.com>
2025-08-10 11:49:45 -0500

    actualizacion en for

---
+++ Ciclo_para/4_impar_1_50.py
@@ -0,0 +1,5 @@
+print("Los numeros impares del 1 al 50 son: ")#Mensaje
+for i in range (1,51):#Del 1 al 51 para contar el 50 pero tambien se puede dejar en 50 ya que es par
+    if i % 2 != 0: #condicion para impar
+        print(i)#imprimo el numero
+
