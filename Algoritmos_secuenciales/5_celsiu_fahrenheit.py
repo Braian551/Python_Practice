2da4ec591b63141895ef1f00e652c299e9f707e1
Braian <braianoquen@gmail.com>
2025-08-10 07:25:49 -0500

    celsiu_fahrenheit.py final

---
+++ Algoritmos_secuenciales/5_celsiu_fahrenheit.py
@@ -0,0 +1,8 @@
+#Programa que convierte grados celsius en fahrenheit
+print("#Programa que convierte grados celsius en fahrenheit")
+
+grad = float(input("Ingresa los grados celsius a convertir: ")) #El input para ingresa los grados a convertir, tipo float
+
+fahren = (1.8 * grad) + 32 #La formula de conversion
+
+print (f"{grad}° celsius son {fahren}° fahrenheit") #Imprimo el resultado
