8b81482da2d203526f7d807b1097c9399aab69d9
Braian <braianoquen@gmail.com>
2025-08-10 09:31:46 -0500

    Tabla del 5

---
+++ Ciclo_para/3_tabla5.py
@@ -0,0 +1,4 @@
+print("Tabla del 5") #Informacion de la tabla
+for i in range(1,11): #no lo hice desde el 0 para no contar necesariamente desde el cero aunque puede, y hasta al 11 para que cuente el 10
+    print(f"5 X {i} = {5*i}") #Imprimo el resultador con el string 5 X y añado la i porque se suma en cada interación, y en el =, soy el resultado multiplicano el 5 con i
+    
