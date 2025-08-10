d1ef96bd96b16e694e8a3ab13fd557776c8f8a2f
Braian <braianoquen@gmail.com>
2025-08-10 07:36:03 -0500

    raiz.py

---
+++ Funciones_matematicas/1_raiz.py
@@ -0,0 +1,7 @@
+import math as mt #importo la libreria math y le doy un alias mt
+
+num = float(input("Ingresa un número: ")) #input para el número tipo float
+
+
+raiz= mt.sqrt(num) #aplicacion de la funcion sqrt para raiz
+print(f"La raiz de {num} es {raiz}") #Imprimo resultado
