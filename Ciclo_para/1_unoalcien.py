ab8c44a36bd8437c1bcc3f431f430bfc05f80e91
Braian <braianoquen@gmail.com>
2025-08-10 09:13:28 -0500

    uno al cien

---
+++ Ciclo_para/1_unoalcien.py
@@ -0,0 +1,3 @@
+#Del uno al cien
+for i in range(1,101): #comenzamos desde el uno hasta el 101 ya que sabemos que para que de 100 tendria que empezar desde cero, pero tiene que empezar desde uno
+    print(i, end=", ") #quito el salto de linea al usar end, y agrego un espacio y coma para que se vea ordenado y no todo de pa bajo
