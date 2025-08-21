20a59de2c391838ed5535f3d2c7c08a651f47d3c
Braian <braianoquen@gmail.com>
2025-08-21 16:23:15 -0500

    logica matematica ahora si terminado

--- Logica_matematica/4_program.py
+++ Logica_matematica/4_program.py
@@ -1,9 +1,9 @@
 # 4.  Un programa descarga archivos desde internet. Parte con una descarga de 6.000 MB. Descarga 450 MB por minuto y borra 150 MB de archivos temporales por minuto. 
 # ¿Cuántos MB habrá descargado efectivamente después de 10 minutos? ¿cuanto espacio le quedaría si el consumo inicial  era la 4 parte de la totalidad de su capacidad? 
 
-descarg_inical = 6000
-descarga = 450
-borra = 150
+descarg_inical = int(input("Ingrese la cantidad de MB iniciales: "))
+descarga = int(input("Ingrese la cantidad de MB que se descargan por minuto: "))
+borra = int(input("Ingrese la cantidad de MB que se borran por minuto: "))
 
 descarga_final = descarg_inical + (descarga * 10) - (borra * 10)
 espacio = descarg_inical * 4
