49f2f345526359e37d9194f72494dd8bdd43a8bd
Braian <braianoquen@gmail.com>
2025-08-29 13:33:14 -0500

    tuplas y extras

---
+++ Tuplas/ejemplo1.py
@@ -0,0 +1,8 @@
+# tuplas solo se crean y consulta pero no se puede hacer nada mas por que es inmutable
+# son inmutables es decir, no eleimi datos, no se puede agregar mas informacion
+#pero permite crear listas en base de los datos que el tiene
+# puedo extrar informacion de su indoice y llevarla a otra variable
+# como se crea??
+
+tupla=(1,2,3,4,5)
+print(tupla)
